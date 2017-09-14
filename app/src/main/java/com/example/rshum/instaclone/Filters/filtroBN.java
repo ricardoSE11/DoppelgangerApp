9e118d9bd9127d5561407bc768d7e8003afbabf6
Fabipercu <31943724+Fabipercu@users.noreply.github.com>
2017-09-14 02:27:05 -0600

    Update filtroBN.java

--- app/src/main/java/com/example/rshum/instaclone/Filters/filtroBN.java
+++ app/src/main/java/com/example/rshum/instaclone/Filters/filtroBN.java
@@ -1,119 +1,135 @@
-package com.fabianbarahona.pruebafiltros;
+public class filtroBN{
+    public Bitmap Averaging(Bitmap src){
+        Bitmap finalImage = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        //Bitmap x =((BitmapDrawable)arepa.getDrawable()).getBitmap();
+        int A,R,G,B;
+        int colorPixel;
+        int width = src.getWidth();
+        int height = src.getHeight();
 
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Build;
-import android.widget.ImageView;
-
-import static java.lang.Integer.*;
-
-public interface IFiltroBN {
-    public Bitmap setPixels();
-}
-
-public abstract class filtro{
-    public Bitmap original =((BitmapDrawable)ImageView.getDrawable()).getBitmap();
-    Bitmap filteredImage = null;
-    int A,R,G,B;
-    int colorPixel;
-    int width =original.getWidth();
-    int height = original.getHeight();
-
-    public void setBitmapOriginal(Bitmap org){
-        this.original=org;
-    }
-}
-
-public class filtroAveraging extends filtro implements IFiltroBN{
-    public filtroAveraging(Bitmap x){
-        setBitmapOriginal(x);
-    }
-    @Override
-    public Bitmap setPixels() {
-        for (int i = 0; i < height; i++) {
-            for (int x = 0; x < width; x++) {
-                colorPixel = original.getPixel(i, x);
+        for (int i=0; i<height; i++){
+            for (int x=0; x<width; x++){
+                colorPixel = src.getPixel(i, x);
                 A = Color.alpha(colorPixel);
                 R = Color.red(colorPixel);
                 G = Color.green(colorPixel);
                 B = Color.blue(colorPixel);
 
-                R = (R + G + B) / 3;
-                G = B = R;
-                filteredImage.setPixel(i, x, Color.argb(A, R, G, B));
+                R=(R+G+B)/3;
+                G=B=R;
+                finalImage.setPixel(i, x, Color.argb(A, R, G, B));
+            }
+        }
+
+        return finalImage;
+    }
+    public Bitmap Desaturation(Bitmap src){
+        Bitmap finalImage = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+
+        int A,R,G,B;
+        int colorPixel;
+        int width = src.getWidth();
+        int height = src.getHeight();
+
+        for (int i=0; i<height; i++){
+            for (int x=0; x<width; x++){
+                colorPixel = src.getPixel(i, x);
+                A = Color.alpha(colorPixel);
+                R = Color.red(colorPixel);
+                G = Color.green(colorPixel);
+                B = Color.blue(colorPixel);
+
+                R=((max((max(R, B)), G)+ min((min(R, B)), G))/2;
+                G=B=R;
+                finalImage.setPixel(i, x, Color.argb(A, R, G, B));
+            }
+        }
+
+        return finalImage;
+    }
+
+
+    public Bitmap MAX(Bitmap src){
+        Bitmap finalImage = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        //Bitmap x =((BitmapDrawable)arepa.getDrawable()).getBitmap();
+        int A,R,G,B;
+        int colorPixel;
+        int width = src.getWidth();
+        int height = src.getHeight();
+
+        for (int i=0; i<height; i++){
+            for (int x=0; x<width; x++){
+                colorPixel = src.getPixel(i, x);
+                A = Color.alpha(colorPixel);
+                R = Color.red(colorPixel);
+                G = Color.green(colorPixel);
+                B = Color.blue(colorPixel);
+
+                R=max((max(R, B)), G);
+                G=B=R;
+                finalImage.setPixel(i, x, Color.argb(A, R, G, B));
+            }
+        }
+
+        return finalImage;
+    }
+    public class filtroBN{
+        public Bitmap Averaging(Bitmap src){
+            Bitmap finalImage = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+            //Bitmap x =((BitmapDrawable)arepa.getDrawable()).getBitmap();
+            int A,R,G,B;
+            int colorPixel;
+            int width = src.getWidth();
+            int height = src.getHeight();
+
+            for (int i=0; i<height; i++){
+                for (int x=0; x<width; x++){
+                    colorPixel = src.getPixel(i, x);
+                    A = Color.alpha(colorPixel);
+                    R = Color.red(colorPixel);
+                    G = Color.green(colorPixel);
+                    B = Color.blue(colorPixel);
+
+                    R=min((min(R, B)), G);
+                    G=B=R;
+                    finalImage.setPixel(i, x, Color.argb(A, R, G, B));
                 }
             }
 
-            return filteredImage;
+            return finalImage;
         }
-    }
-public class filtroDesaturarion extends filtro implements IFiltroBN{
-    public filtroDesaturarion(Bitmap x){
-        setBitmapOriginal(x);
-    }
-    @Override
-    public Bitmap setPixels() {
-        for (int i = 0; i < height; i++) {
-            for (int x = 0; x < width; x++) {
-                colorPixel = original.getPixel(i, x);
-                A = Color.alpha(colorPixel);
-                R = Color.red(colorPixel);
-                G = Color.green(colorPixel);
-                B = Color.blue(colorPixel);
 
-                R = (max((max(R, B)), G)+ min((min(R, B)), G))/2;
-                G = B = R;
-                filteredImage.setPixel(i, x, Color.argb(A, R, G, B));
-            }
+        public static Bitmap applyGaussianBlur(Bitmap src) {
+            //set gaussian blur configuration
+            double[][] GaussianBlurConfig = new double[][] {
+                    { 1, 2, 1 },
+                    { 2, 4, 2 },
+                    { 1, 2, 1 }
+            };
+            // create instance of Convolution matrix
+            ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+            // Apply Configuration
+            convMatrix.applyConfig(GaussianBlurConfig);
+            convMatrix.Factor = 16;
+            convMatrix.Offset = 0;
+            //return out put bitmap
+            return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
         }
-        return filteredImage;
-    }
-
-    //return finalImage;
-}
-
-public class filtroMAX extends filtro implements IFiltroBN{
-    public filtroMAX(Bitmap x){
-        setBitmapOriginal(x);
-    }
-    public Bitmap setPixels() {
-        for (int i = 0; i < height; i++) {
-            for (int x = 0; x < width; x++) {
-                colorPixel = original.getPixel(i, x);
-                A = Color.alpha(colorPixel);
-                R = Color.red(colorPixel);
-                G = Color.green(colorPixel);
-                B = Color.blue(colorPixel);
-
-                R = max((max(R, B)), G);
-                G = B = R;
-                filteredImage.setPixel(i, x, Color.argb(A, R, G, B));
-            }
+        public static Bitmap applyEmboss(Bitmap src) {
+            //set gaussian blur configuration
+            double[][] GaussianBlurConfig = new double[][] {
+                    { -2, -1, 0 },
+                    { -1, 1, 1 },
+                    { 0, 1, 2 }
+            };
+            // create instance of Convolution matrix
+            ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+            // Apply Configuration
+            convMatrix.applyConfig(GaussianBlurConfig);
+            convMatrix.Factor = 16;
+            convMatrix.Offset = 0;
+            //return out put bitmap
+            return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
         }
-        return filteredImage;
-    }
-}
-public class filtroMIN extends filtro implements IFiltroBN{
-    public filtroMIN(Bitmap x){
-        setBitmapOriginal(x);
-    }
-    public Bitmap setPixels() {
-        for (int i = 0; i < height; i++) {
-            for (int x = 0; x < width; x++) {
-                colorPixel = original.getPixel(i, x);
-                A = Color.alpha(colorPixel);
-                R = Color.red(colorPixel);
-                G = Color.green(colorPixel);
-                B = Color.blue(colorPixel);
 
-
-                R = min((min(R, B)), G);
-
-                G = B = R;
-                filteredImage.setPixel(i, x, Color.argb(A, R, G, B));
-            }
-        }
-        return filteredImage;
-    }
 }
